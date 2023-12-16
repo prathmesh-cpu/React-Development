2dae21eb396e82861a0ffc58a5cfbabcb11d071b
Prathamesh Dhavale <dhawaleprathmesh2003@gmail.com>
2023-12-16 23:18:25 +0530

    Modify file

--- Chapter 02 - Igniting our App/App.js
+++ Chapter 02 - Igniting our App/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 let h1 = React.createElement("h1", {}, "Hello World");
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);
